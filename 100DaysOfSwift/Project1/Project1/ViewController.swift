25ba785f1dd1c650553f91e8eb641aec1a2f3e1d
Carlos Álvaro <carlos@cdalvaro.io>
2019-03-17 20:04:01 +0100

    Project1 - Get images path

--- 100DaysOfSwift/Project1/Project1/ViewController.swift
+++ 100DaysOfSwift/Project1/Project1/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // This is a picture to load!
+                pictures.append(item)
+            }
+        }
     }
-
-
+    
 }
-
