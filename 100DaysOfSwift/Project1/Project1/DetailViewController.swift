24b5c4299c8510ae75bc2af5f0a85ff279a49174
Carlos Álvaro <carlos@cdalvaro.io>
2019-03-24 11:24:17 +0100

    Project 1 - Set view controllers title

--- 100DaysOfSwift/Project1/Project1/DetailViewController.swift
+++ 100DaysOfSwift/Project1/Project1/DetailViewController.swift
@@ -15,6 +15,8 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = selectedImage
 
         // Do any additional setup after loading the view.
         if let imageToLoad = selectedImage {
--- 100DaysOfSwift/Project1/Project1/ViewController.swift
+++ 100DaysOfSwift/Project1/Project1/ViewController.swift
@@ -16,7 +16,8 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        title = "Storm Viewer"
         
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
