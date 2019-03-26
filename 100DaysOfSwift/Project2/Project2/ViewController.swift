95659b816435e76d7fa64913853dbf06fba71c68
Carlos Álvaro <carlos@cdalvaro.io>
2019-03-26 20:14:34 +0100

    Project 2 - Designing the layout

--- 100DaysOfSwift/Project2/Project2/ViewController.swift
+++ 100DaysOfSwift/Project2/Project2/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
