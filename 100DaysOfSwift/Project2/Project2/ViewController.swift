096a0612feee5cc862e153d78e193751417c3450
Carlos Álvaro <carlos@cdalvaro.io>
2019-03-27 19:12:12 +0100

    Project 2 - Add logic for checking the answer

--- 100DaysOfSwift/Project2/Project2/ViewController.swift
+++ 100DaysOfSwift/Project2/Project2/ViewController.swift
@@ -39,7 +39,7 @@ class ViewController: UIViewController {
         askQuestion()
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
         
@@ -48,6 +48,23 @@ class ViewController: UIViewController {
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
         title = countries[correctAnswer].uppercased()
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
     }
     
 }
