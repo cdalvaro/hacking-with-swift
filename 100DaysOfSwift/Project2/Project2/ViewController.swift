145e648e68a26d19433986a76ff62daf94789928
Carlos Álvaro <carlos@cdalvaro.io>
2019-03-27 19:23:59 +0100

    Project 2 - Show score in the navigation bar

--- 100DaysOfSwift/Project2/Project2/ViewController.swift
+++ 100DaysOfSwift/Project2/Project2/ViewController.swift
@@ -47,7 +47,7 @@ class ViewController: UIViewController {
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
-        title = countries[correctAnswer].uppercased()
+        title = "\(countries[correctAnswer].uppercased()) - Score: \(score)"
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
