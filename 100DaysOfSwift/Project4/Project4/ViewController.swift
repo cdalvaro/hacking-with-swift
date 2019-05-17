914dd3264ecbeef7cbc8903cc8e5e86d2c92c4c5
Carlos Álvaro <carlos@cdalvaro.io>
2019-05-17 19:04:12 +0200

    Project 4 - Set WKWebView as main view

--- 100DaysOfSwift/Project4/Project4/ViewController.swift
+++ 100DaysOfSwift/Project4/Project4/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
-
 
 }
 
