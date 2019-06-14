7ebd664b06595a3a56772278f2f95faf24b91522
Carlos Álvaro <carlos@cdalvaro.io>
2019-06-14 20:35:30 +0200

    Project 5 - Checking for valid answers

--- 100DaysOfSwift/Project5/Project5/ViewController.swift
+++ 100DaysOfSwift/Project5/Project5/ViewController.swift
@@ -76,14 +76,28 @@ class ViewController: UITableViewController {
     }
     
     func isPossible(word: String) -> Bool {
+        guard var tempWord = title?.lowercased() else { return false }
+        
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        
         return true
     }
     
     func isOriginal(word: String) -> Bool {
-        return true
+        return !usedWords.contains(word)
     }
     
     func isReal(word: String) -> Bool {
-        return true
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
     }
 }
