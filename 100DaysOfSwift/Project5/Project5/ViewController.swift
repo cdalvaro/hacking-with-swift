046e30ecfd51d27f42a4a1264a9307e92912e53f
Carlos Álvaro <carlos@cdalvaro.io>
2019-06-14 17:07:04 +0200

    Project 5 - Setting up

--- 100DaysOfSwift/Project5/Project5/ViewController.swift
+++ 100DaysOfSwift/Project5/Project5/ViewController.swift
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
-
