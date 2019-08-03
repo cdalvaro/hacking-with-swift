844dc40ef91390c0ead3d82f2f320cbb4abfcf26
Carlos Álvaro <carlos@cdalvaro.io>
2019-08-03 18:38:50 +0200

    Project 6b - Challenge #3

--- 100DaysOfSwift/Project6b/Project6b/ViewController.swift
+++ 100DaysOfSwift/Project6b/Project6b/ViewController.swift
@@ -50,7 +50,8 @@ class ViewController: UIViewController {
             
             label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
             label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor,
+                                          multiplier: 0.2, constant: -10).isActive = true
             
             if let previous = previous {
                 // we have a previous label - create a height constraint
