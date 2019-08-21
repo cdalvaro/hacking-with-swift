3bbd78abdef2a8db9775b36cac2884c19e7d880a
Carlos Álvaro <carlos@cdalvaro.io>
2019-08-21 17:37:05 +0200

    Project 7 - Creating the basic UI: UITabBarController

---
+++ 100DaysOfSwift/Project7/Project7/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Carlos David on 20/08/2019.
+//  Copyright © 2019 cdalvaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var petitions = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        
+        return cell
+    }
+}
