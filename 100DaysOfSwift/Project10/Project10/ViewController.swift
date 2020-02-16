52ba3991cba31a23601dd630c3dcb0d153fd9a68
Carlos Álvaro <carlos@cdalvaro.io>
2020-02-16 09:44:50 +0100

    100 Days of Swift - Day 42
    Project 10 - Part one
    
    Setting up the project

---
+++ 100DaysOfSwift/Project10/Project10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Carlos David on 16/02/2020.
+//  Copyright © 2020 cdalvaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
