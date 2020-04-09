c1ff3bec72a8326c00307320f719d9136483e278
Carlos Álvaro <carlos@cdalvaro.io>
2020-04-09 10:56:53 +0200

    100 Days of Swift - Day 47
    Project 11 - Part three
    
    Special effects: SKEmitterNode

--- 100DaysOfSwift/Project11/Project11/GameScene.swift
+++ 100DaysOfSwift/Project11/Project11/GameScene.swift
@@ -145,6 +145,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func destroy(ball: SKNode) {
+        if let fireParticles = SKEmitterNode(fileNamed: "FireParticles") {
+            fireParticles.position = ball.position
+            addChild(fireParticles)
+        }
+        
         ball.removeFromParent()
     }
     
