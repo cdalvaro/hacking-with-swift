72d521628aaca1a25d20245fdd60308d9273fbf1
Carlos Álvaro <carlos@cdalvaro.io>
2020-04-17 19:41:18 +0200

    100 Days of Swift - Day 49
    Project 12 - Part two
    
    Challenge #3 - Modify project 5 so that it saves the current word and all the player’s entries to UserDefaults, then loads them back when the app launches.

---
+++ 100DaysOfSwift/Project5-UserDefaults/Project5-UserDefaults/ViewController.swift
@@ -0,0 +1,154 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Carlos David on 11/06/2019.
+//  Copyright © 2019 cdalvaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                            target: self,
+                                                            action: #selector(promptForAnswer))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh,
+                                                           target: self,
+                                                           action: #selector(startGame))
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        let defaults = UserDefaults.standard
+        if let lastWord = defaults.string(forKey: "lastWord"), !lastWord.isEmpty {
+            title = lastWord
+            usedWords = defaults.object(forKey: "usedWords") as? [String] ?? [String]()
+        } else {
+            startGame()
+        }
+    }
+    
+    @objc func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData() // Forces to recall numberOfRowsInSection and cellForRowAt
+        removePreviousGame()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        let lowerAnswer = answer.lowercased()
+        
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usedWords.insert(lowerAnswer, at: 0)
+                    save()
+                    
+                    //  This is an optimization in order to not reload the whole tableview
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                } else {
+                    showErrorMessage(title: "Word not recognized",
+                                     message: "You can't just make them up, you know!")
+                }
+            } else {
+                showErrorMessage(title: "Word already used",
+                                 message: "Be more original!")
+            }
+        } else {
+            showErrorMessage(title: "Word not possible",
+                             message: "You can't spell that word from \(title!.lowercased()).")
+        }
+    }
+    
+    func isPossible(word: String) -> Bool {
+        guard var tempWord = title?.lowercased() else { return false }
+        
+        if word == tempWord {
+            return false
+        }
+        
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isReal(word: String) -> Bool {
+        if word.count < 3 {
+            return false
+        }
+        
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func showErrorMessage(title: String, message: String) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func save() {
+        let defaults = UserDefaults.standard
+        defaults.set(title, forKey: "lastWord")
+        defaults.set(usedWords, forKey: "usedWords")
+    }
+    
+    func removePreviousGame() {
+        let defaults = UserDefaults.standard
+        defaults.removeObject(forKey: "lastWord")
+        defaults.removeObject(forKey: "usedWords")
+    }
+}
