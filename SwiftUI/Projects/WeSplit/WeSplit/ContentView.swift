9da4aa00849906f933514e24a492f16543b2d05a
Carlos Álvaro <carlos@cdalvaro.io>
2020-08-10 20:13:27 +0200

    100 Days of SwiftUI - Day 17
    Project 1 - Part two
    
    Reading text from the user with TextField

--- SwiftUI/Projects/WeSplit/WeSplit/ContentView.swift
+++ SwiftUI/Projects/WeSplit/WeSplit/ContentView.swift
@@ -8,43 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
-    @State private var name = ""
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
     
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = 0
+    let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
         // Creating a form and adding a navigation bar
-        NavigationView {
-            Form {
-                Section {
-                    Text("Hello World")
-                }
-                
-                // Modifying program state: @State
-                Section {
-                    Button("Tap Count: \(tapCount)") {
-                        self.tapCount += 1
-                    }
-                }
-                
-                // Binding state to user interface controls
-                Section {
-                    TextField("Enter your name", text: $name)
-                    Text("Your name is \(name)")
-                }
-                
-                Section {
-                    Picker("Select your student", selection: $selectedStudent) {
-                        ForEach(0 ..< students.count) {
-                            Text(self.students[$0])
-                        }
-                    }
-                    Text("You chose: Student \(students[selectedStudent])")
-                }
+        Form {
+            Section {
+                TextField("Amount", text: $checkAmount)
+                    .keyboardType(.decimalPad)
             }
-            .navigationBarTitle(Text("SwiftUI"))
+            
+            Section {
+                Text("$\(checkAmount)")
+            }
         }
     }
 }
