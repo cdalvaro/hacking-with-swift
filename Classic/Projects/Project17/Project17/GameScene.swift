21c0e001c48b78211432a895e605273f7963499a
Carlos Álvaro <carlos@cdalvaro.io>
2021-02-13 13:51:25 +0100

    100 Days of Swift - Day 62
    Project 17 - Part one
    
    Setting up

---
+++ Classic/Projects/Project17/Project17/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Carlos David on 13/2/21.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {}
+
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
