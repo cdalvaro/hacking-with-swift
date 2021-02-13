ee91ab6c3587dbd2d652593386719425c8197385
Carlos Álvaro <carlos@cdalvaro.io>
2021-02-13 13:59:57 +0100

    100 Days of Swift - Day 62
    Project 17 - Part one
    
    Space: the final frontier

--- Classic/Projects/Project17/Project17/GameScene.swift
+++ Classic/Projects/Project17/Project17/GameScene.swift
@@ -7,8 +7,42 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
-    override func didMove(to view: SKView) {}
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var starfield: SKEmitterNode!
+    var player: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
+
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+
+    override func didMove(to view: SKView) {
+        backgroundColor = .black
+
+        starfield = SKEmitterNode(fileNamed: "starfield")
+        starfield.position = CGPoint(x: 1024, y: 384)
+        starfield.advanceSimulationTime(10)
+        addChild(starfield)
+        starfield.zPosition = -1
+
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384)
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.contactTestBitMask = 1
+        addChild(player)
+
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
+
+        score = 0
+
+        physicsWorld.gravity = .zero
+        physicsWorld.contactDelegate = self
+    }
 
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
