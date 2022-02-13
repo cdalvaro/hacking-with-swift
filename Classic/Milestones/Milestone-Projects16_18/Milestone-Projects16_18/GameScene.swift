fb23b1a065dd4f9739f3910c7f9f0368b1cb7b24
Carlos Álvaro <carlos@cdalvaro.io>
2022-02-13 17:43:05 +0100

    100 Days of Swift - Day 66
    Milestone: Projects 16-18 Challenge
    
    - Handle touches

--- Classic/Milestones/Milestone-Projects16_18/Milestone-Projects16_18/GameScene.swift
+++ Classic/Milestones/Milestone-Projects16_18/Milestone-Projects16_18/GameScene.swift
@@ -5,8 +5,8 @@
 //  Created by Carlos Álvaro on 23/1/22.
 //
 
-import SpriteKit
 import GameplayKit
+import SpriteKit
 
 @propertyWrapper
 struct Score {
@@ -23,8 +23,7 @@ struct Score {
 }
 
 class GameScene: SKScene {
-    
-    private var scoreLabel : SKLabelNode!
+    private var scoreLabel: SKLabelNode!
     @Score private var score {
         didSet {
             scoreLabel.text = "Score: \(score)"
@@ -32,6 +31,7 @@ class GameScene: SKScene {
     }
 
     private var bulletsSprite: SKSpriteNode!
+    private var reloadLabel: SKLabelNode!
 
     private var bulletTextures = [
         SKTexture(imageNamed: "shots0"),
@@ -51,7 +51,7 @@ class GameScene: SKScene {
     private var targetCreated = 0
 
     private var isGameOver = false
-    
+
     override func didMove(to view: SKView) {
         createBackground()
         createOverlay()
@@ -81,7 +81,7 @@ class GameScene: SKScene {
 
         scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
         scoreLabel.horizontalAlignmentMode = .right
-        scoreLabel.position = CGPoint(x: 680, y: 50)
+        scoreLabel.position = CGPoint(x: 680, y: 60)
         scoreLabel.zPosition = 500
         scoreLabel.text = "Score: 0"
         addChild(scoreLabel)
@@ -90,6 +90,14 @@ class GameScene: SKScene {
         bulletsSprite.position = CGPoint(x: 170, y: 70)
         bulletsSprite.zPosition = 500
         addChild(bulletsSprite)
+
+        reloadLabel = SKLabelNode(fontNamed: "Chalkduster")
+        reloadLabel.horizontalAlignmentMode = .right
+        reloadLabel.position = CGPoint(x: 350, y: 60)
+        reloadLabel.zPosition = 500
+        reloadLabel.text = "Reload"
+        reloadLabel.name = "reload"
+        addChild(reloadLabel)
     }
 
     func createWater() {
@@ -149,9 +157,8 @@ class GameScene: SKScene {
 
     func createTarget() {
         let target = Target()
-        target.setup()
 
-        let level = Int.random(in: 0...2)
+        let level = Int.random(in: 0 ... 2)
         var movingRight = true
 
         switch level {
@@ -188,37 +195,48 @@ class GameScene: SKScene {
 
         levelUp()
     }
-    
-    func touchDown(atPoint pos : CGPoint) {
 
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
+    func shot(at location: CGPoint) {
+        let hitNodes = nodes(at: location).filter { $0.name == "target" }
 
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
+        guard let hitNode = hitNodes.first else { return }
+        guard let parentNode = hitNode.parent as? Target else { return }
 
+        parentNode.hit()
+        score += 3
     }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if isGameOver {
+            if let newGame = SKScene(fileNamed: "GameScene") {
+                let transition = SKTransition.doorway(withDuration: 1)
+                view?.presentScene(newGame, transition: transition)
+            }
+        } else {
+            for touch in touches {
+                let location = touch.location(in: self)
 
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+                let hitNodes = nodes(at: location).filter { $0.name == "reload" }
+                if !hitNodes.isEmpty {
+                    run(SKAction.playSoundFileNamed("reload.wav", waitForCompletion: false))
+                    bulletsInClip = 3
+                    score -= 1
+                    continue
+                }
 
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+                if bulletsInClip > 0 {
+                    run(SKAction.playSoundFileNamed("shot.wav", waitForCompletion: false))
+                    bulletsInClip -= 1
+                    shot(at: location)
+                } else {
+                    run(SKAction.playSoundFileNamed("empty.wav", waitForCompletion: false))
 
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+                    let setRed = SKAction.colorize(with: .red, colorBlendFactor: 1, duration: 0.3)
+                    let restore = setRed.reversed()
+                    let sequence = SKAction.sequence([setRed, restore])
+                    reloadLabel.run(sequence)
+                }
+            }
+        }
     }
 }
--- Classic/Milestones/Milestone-Projects16_18/Milestone-Projects16_18/Target.swift
+++ Classic/Milestones/Milestone-Projects16_18/Milestone-Projects16_18/Target.swift
@@ -11,6 +11,15 @@ class Target: SKNode {
     private var target: SKSpriteNode!
     private var stick: SKSpriteNode!
 
+    override init() {
+        super.init()
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func setup() {
         let stickType = Int.random(in: 0...2)
         let targetType = Int.random(in: 0...3)
