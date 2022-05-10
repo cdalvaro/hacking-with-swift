ba77a6a3b87120e3e6d9fbc58d706d38e9cf899f
Carlos Álvaro <carlos@cdalvaro.io>
2022-05-10 18:13:42 +0200

    100 Days of SwiftUI - Day 27
    Project 4 - Part two
    
    Connecting SwiftUI to Core ML

--- SwiftUI/Projects/BetterRest/BetterRest/ContentView.swift
+++ SwiftUI/Projects/BetterRest/BetterRest/ContentView.swift
@@ -5,12 +5,17 @@
 //  Created by Carlos Álvaro on 23/1/22.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
 
     var body: some View {
         NavigationView {
@@ -34,10 +39,36 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateBedtime)
             }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
 
-    func calculateBedtime() {}
+    func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 3600
+            let minute = (components.minute ?? 0) * 60
+
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+
+            let sleepTime = wakeUp - prediction.actualSleep
+
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime. \(error)"
+        }
+
+        showingAlert = true
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
