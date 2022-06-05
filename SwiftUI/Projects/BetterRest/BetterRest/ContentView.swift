e438f6ba9a48893d51fc8fcc5fc9d9b030ae8d0b
Carlos Álvaro <carlos@cdalvaro.io>
2022-06-05 12:27:47 +0200

    100 Days of SwiftUI - Day 28
    Project 4 - Part three
    
    Replace the “Number of cups” stepper with a Picker showing the same range of values.

--- SwiftUI/Projects/BetterRest/BetterRest/ContentView.swift
+++ SwiftUI/Projects/BetterRest/BetterRest/ContentView.swift
@@ -37,7 +37,11 @@ struct ContentView: View {
                 }
 
                 Section("Daily coffee intake") {
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                    Picker("Number of cups", selection: $coffeeAmount) {
+                        ForEach(1..<21) {
+                            Text($0 == 1 ? "1 cup" : "\($0) cups")
+                        }
+                    }
                 }
             }
             .navigationTitle("BetterRest")
