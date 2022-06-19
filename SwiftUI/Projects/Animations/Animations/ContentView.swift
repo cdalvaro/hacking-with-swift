816ea585e184d8b74ab26989a7fc9c425539f8b7
Carlos Álvaro <carlos@cdalvaro.io>
2022-06-19 14:39:35 +0200

    100 Days of SwiftUI - Day 32
    Project 6 - Part one
    
    Animating bindings

--- SwiftUI/Projects/Animations/Animations/ContentView.swift
+++ SwiftUI/Projects/Animations/Animations/ContentView.swift
@@ -11,36 +11,23 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
 
     var body: some View {
-        Button("Tap me") {
-//            animationAmount += 1
+        VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1 ... 10)
+
+            Spacer()
+
+            Button("Tap me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeInOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
-        }
-//        .scaleEffect(animationAmount)
-//        .blur(radius: (animationAmount - 1) * 3)
-//        .animation(.default, value: animationAmount)
-//        .animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount)
-//        .animation(
-//            .easeInOut(duration: 1)
-//                .repeatCount(3, autoreverses: true),
-//            value: animationAmount
-//        )
     }
 }
 
--- SwiftUI/README.md
+++ SwiftUI/README.md
@@ -276,5 +276,6 @@ So, if the app chose _“Rock”_ and _“Win”_ the player would need to choos
 
 - Creating implicit animations
 - Customizing animations in SwiftUI
+- Animating bindings
 
 [Code](Projects/Animations)
