f59389d9c28a3b59ecfea7f57aba7050c015e271
Carlos Álvaro <carlos@cdalvaro.io>
2022-06-27 17:19:59 +0200

    100 Days of SwiftUI - Day 33
    Project 6 - Part two
    
    Animating gestures

--- SwiftUI/Projects/Animations/Animations/ContentView.swift
+++ SwiftUI/Projects/Animations/Animations/ContentView.swift
@@ -8,19 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("Hello, SwiftUI!")
     @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
 
     var body: some View {
-        Button("Tap me") {
-            enabled.toggle()
+        HStack(spacing: 0) {
+            ForEach(0 ..< letters.count) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(
+                        .default.delay(Double(num) / 20),
+                        value: dragAmount
+                    )
+            }
+            .gesture(DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                })
         }
-        .frame(width: 200, height: 200)
-        .background(enabled ? .blue : .red)
-        .animation(.default, value: enabled)
-        .foregroundColor(.white)
-        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
-        // animation calling order matters!!
     }
 }
 
