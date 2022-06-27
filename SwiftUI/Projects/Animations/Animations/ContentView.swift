de66dfa8c52fdca8acdf1e4834e97ef8f74193bc
Carlos Álvaro <carlos@cdalvaro.io>
2022-06-27 17:24:45 +0200

    100 Days of SwiftUI - Day 33
    Project 6 - Part two
    
    Showing and hiding views with transitions

--- SwiftUI/Projects/Animations/Animations/ContentView.swift
+++ SwiftUI/Projects/Animations/Animations/ContentView.swift
@@ -8,29 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    let letters = Array("Hello, SwiftUI!")
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
+    @State private var isShowingRed = false
 
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0 ..< letters.count) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? .blue : .red)
-                    .offset(dragAmount)
-                    .animation(
-                        .default.delay(Double(num) / 20),
-                        value: dragAmount
-                    )
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
             }
-            .gesture(DragGesture()
-                .onChanged { dragAmount = $0.translation }
-                .onEnded { _ in
-                    dragAmount = .zero
-                    enabled.toggle()
-                })
+
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
         }
     }
 }
