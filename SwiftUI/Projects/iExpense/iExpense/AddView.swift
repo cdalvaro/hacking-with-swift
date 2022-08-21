3f7622b8fa8d7e264c3cd415fcd1b6f2fe53bb18
Carlos Álvaro <carlos@cdalvaro.io>
2022-08-21 20:24:39 +0200

    100 Days of SwiftUI - Day 37
    Project 7 - Part two
    
    Final polish

--- SwiftUI/Projects/iExpense/iExpense/AddView.swift
+++ SwiftUI/Projects/iExpense/iExpense/AddView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct AddView: View {
     @ObservedObject var expenses: Expenses
+    @Environment(\.dismiss) var dismiss
 
     @State private var name = ""
     @State private var type = "Personal"
@@ -35,6 +36,7 @@ struct AddView: View {
                 Button("Save") {
                     let item = ExpenseItem(name: name, type: type, amount: amount)
                     expenses.items.append(item)
+                    dismiss()
                 }
             }
         }
--- SwiftUI/Projects/iExpense/iExpense/ContentView.swift
+++ SwiftUI/Projects/iExpense/iExpense/ContentView.swift
@@ -15,7 +15,16 @@ struct ContentView: View {
         NavigationView {
             List {
                 ForEach(expenses.items, id: \.id) { item in
-                    Text(item.name)
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name).font(.headline)
+                            Text(item.type)
+                        }
+
+                        Spacer()
+
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
                 }
                 .onDelete(perform: removeItems)
             }
