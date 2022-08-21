c3b320f72a05967081a368b556a6f3e2d9564f5e
Carlos Álvaro <carlos@cdalvaro.io>
2022-08-21 20:01:58 +0200

    100 Days of SwiftUI - Day 37
    Project 7 - Part two
    
    Sharing an observed object with a new view

---
+++ SwiftUI/Projects/iExpense/iExpense/AddView.swift
@@ -0,0 +1,42 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Carlos Álvaro on 21/8/22.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @ObservedObject var expenses: Expenses
+
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+
+    let types = ["Business", "Personal"]
+
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
--- SwiftUI/Projects/iExpense/iExpense/ContentView.swift
+++ SwiftUI/Projects/iExpense/iExpense/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
 
     var body: some View {
         NavigationView {
@@ -21,11 +22,13 @@ struct ContentView: View {
             .navigationTitle("iExpense")
             .toolbar {
                 Button {
-                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                    expenses.items.append(expense)
+                    showingAddExpense = true
                 } label: {
                     Image(systemName: "plus")
                 }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
             }
         }
     }
