e43d2756bc72d6daa889fbcaba211bc798130358
Carlos Álvaro <carlos@cdalvaro.io>
2023-02-11 11:45:50 +0100

    100 Days of SwiftUI - Day 46
    Project 9 - Part four
    
    Create a ColorCyclingRectangle shape that is the rectangular cousin of
    ColorCyclingCircle

--- SwiftUI/Projects/Drawing/Drawing/ContentView.swift
+++ SwiftUI/Projects/Drawing/Drawing/ContentView.swift
@@ -7,59 +7,73 @@
 
 import SwiftUI
 
-struct Arrow: Shape {
-    var strokeWidthWeight: Double {
-        didSet {
-            strokeWidthWeight = max(min(strokeWidthWeight, 1.0), 0.0)
+struct ColorCyclingRectangle: View {
+    @State private var steps: Int = 100
+    @State private var gradientStartPoint: UnitPoint = .top
+    @State private var gradientEndPoint: UnitPoint = .bottom
+
+    var amount = 0.0
+
+    var body: some View {
+        GeometryReader { geo in
+            ZStack {
+                ForEach(Array(stride(from: 0, to: steps, by: 1)), id: \.self) { value in
+                    RoundedRectangle(cornerRadius: 5)
+                        .inset(by: Double(value))
+                        .strokeBorder(
+                            LinearGradient(
+                                gradient: Gradient(colors: [
+                                    color(for: value, brightness: 1),
+                                    color(for: value, brightness: 0.5)
+                                ]),
+                                startPoint: gradientStartPoint, endPoint: gradientEndPoint),
+                            lineWidth: 2)
+                }
+            }
+            .drawingGroup()
+            .onAppear {
+                updateStepsForSize(geo.size)
+            }
+            .onTapGesture { tap in
+                updateGradientPoints(tap: tap, size: geo.size)
+            }
         }
     }
 
-    var strokeHeightWeight: Double {
-        didSet {
-            strokeHeightWeight = max(min(strokeHeightWeight, 1.0), 0.0)
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+
+        if targetHue > 1 {
+            targetHue -= 1
         }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 
-    var animatableData: AnimatablePair<Double, Double> {
-        get { AnimatablePair(strokeWidthWeight, strokeHeightWeight) }
-        set {
-            strokeWidthWeight = newValue.first
-            strokeHeightWeight = newValue.second
-        }
+    func updateStepsForSize(_ size: CGSize) {
+        steps = Int(max(size.height, size.width) / 2.0)
     }
 
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-
-        let arrowWeight = 1.0 - strokeHeightWeight
-        let strokeWidth = rect.width * strokeWidthWeight
-
-        path.move(to: CGPoint(x: rect.minX, y: rect.minY + rect.height * (strokeHeightWeight + arrowWeight / 2.0)))
-        path.addLine(to: CGPoint(x: rect.minX + strokeWidth, y: rect.minY + rect.height * (strokeHeightWeight + arrowWeight / 2.0)))
-        path.addLine(to: CGPoint(x: rect.minX + strokeWidth, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
-        path.addLine(to: CGPoint(x: rect.minX + strokeWidth, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX + strokeWidth, y: rect.minY + rect.height * arrowWeight / 2.0))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY + rect.height * arrowWeight / 2.0))
-
-        return path
+    func updateGradientPoints(tap: CGPoint, size: CGSize) {
+        swap(&gradientStartPoint, &gradientEndPoint)
+        gradientEndPoint = UnitPoint(x: tap.x / size.width,
+                                     y: tap.y / size.height)
     }
 }
 
 struct ContentView: View {
-    @State private var strokeWidthWeight = 0.7
-    @State private var strokeHeightWeight = 0.4
+    @State private var colorCycle = 0.0
 
     var body: some View {
-        Arrow(strokeWidthWeight: strokeWidthWeight, strokeHeightWeight: strokeHeightWeight)
-            .frame(width: 200, height: 100)
-            .onTapGesture {
-                withAnimation {
-                    strokeWidthWeight = Double.random(in: 0.1...0.9)
-                    strokeHeightWeight = Double.random(in: 0.1...0.9)
-                }
-            }
-            .foregroundColor(.yellow)
+        VStack {
+            Spacer()
+            ColorCyclingRectangle(amount: colorCycle)
+                .frame(width: 300, height: 300)
+            Spacer()
+            Slider(value: $colorCycle)
+            Spacer()
+        }
+        .padding()
     }
 }
 
