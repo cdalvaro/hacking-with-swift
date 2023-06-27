f4d0fbe25f5c6562b866cee03dfd6059d98dfe19
Carlos Álvaro <carlos.alvaro@meteologica.com>
2023-06-27 16:09:58 +0200

    100 Days of SwiftUI - Day 49
    Project 10 - Part one
    
    Adding `Codable` conformance for `@Published` properties

---
+++ SwiftUI/Projects/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Carlos Álvaro on 27/6/23.
+//
+
+import SwiftUI
+
+class User: ObservableObject, Codable {
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    @Published var name = "Carlos David"
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/Projects/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Carlos Álvaro on 27/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
