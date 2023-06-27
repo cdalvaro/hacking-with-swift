0ba3ba157be5ea3505d74eecbcba0f1b70053d8a
Carlos Álvaro <carlos.alvaro@meteologica.com>
2023-06-27 16:36:52 +0200

    100 Days of SwiftUI - Day 49
    Project 10 - Part one
    
    Sending and receiving `Codable` data with `URLSession` and SwiftUI

--- SwiftUI/Projects/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ SwiftUI/Projects/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,33 +7,46 @@
 
 import SwiftUI
 
-class User: ObservableObject, Codable {
-    enum CodingKeys: CodingKey {
-        case name
-    }
-    
-    @Published var name = "Carlos David"
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
 }
 
 struct ContentView: View {
+    @State private var results = [Result]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(results, id: \.trackId) { track in
+            VStack(alignment: .leading) {
+                Text(track.trackName)
+                    .font(.headline)
+                Text(track.collectionName)
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data: \(error)")
+        }
     }
 }
 
