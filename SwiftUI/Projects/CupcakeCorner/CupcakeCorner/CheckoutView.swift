4e8e83d746d99cd8b82eee3abeb341406ae12940
Carlos Álvaro <carlos.alvaro@meteologica.com>
2023-06-29 19:11:15 +0200

    100 Days of SwiftUI - Day 51
    Project 10 - Part three
    
    Sending and receiving orders over the internet

--- SwiftUI/Projects/CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ SwiftUI/Projects/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -26,13 +29,42 @@ struct CheckoutView: View {
                     .font(.title)
                 
                 Button("Place order") {
-                    
+                    Task {
+                        await placeOrder()
+                    }
                 }
                 .padding()
             }
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed.")
+        }
     }
 }
 
