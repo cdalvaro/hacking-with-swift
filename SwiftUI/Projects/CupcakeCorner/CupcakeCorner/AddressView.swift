bf20e2c3d148b1418c8f5dbcd7a7337fb7ce880a
Carlos Álvaro <carlos.alvaro@meteologica.com>
2023-06-30 12:30:00 +0200

    100 Days of SwiftUI - Day 52
    Project 10 - Part four
    
    Improve the validation to make sure a string of pure whitespace is invalid.

--- SwiftUI/Projects/CupcakeCorner/CupcakeCorner/AddressView.swift
+++ SwiftUI/Projects/CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -10,13 +10,42 @@ import SwiftUI
 struct AddressView: View {
     @ObservedObject var order: Order
     
+    @FocusState private var nameTextFieldIsFocus: Bool
+    @FocusState private var streetAddressTextFieldIsFocus: Bool
+    @FocusState private var cityTextFieldIsFocus: Bool
+    @FocusState private var zipTextFieldIsFocus: Bool
+    
     var body: some View {
         Form {
             Section {
                 TextField("Name", text: $order.name)
+                    .focused($nameTextFieldIsFocus)
+                    .onChange(of: nameTextFieldIsFocus) { _, isFocused in
+                        if !isFocused {
+                            order.name = trimWhitespaces(order.name)
+                        }
+                    }
                 TextField("Street address", text: $order.streetAddress)
+                    .focused($streetAddressTextFieldIsFocus)
+                    .onChange(of: streetAddressTextFieldIsFocus) { _, isFocused in
+                        if !isFocused {
+                            order.streetAddress = trimWhitespaces(order.streetAddress)
+                        }
+                    }
                 TextField("City", text: $order.city)
+                    .focused($cityTextFieldIsFocus)
+                    .onChange(of: cityTextFieldIsFocus) { _, isFocused in
+                        if !isFocused {
+                            order.city = trimWhitespaces(order.city)
+                        }
+                    }
                 TextField("Zip", text: $order.zip)
+                    .focused($zipTextFieldIsFocus)
+                    .onChange(of: zipTextFieldIsFocus) { _, isFocused in
+                        if !isFocused {
+                            order.zip = trimWhitespaces(order.zip)
+                        }
+                    }
             }
             
             Section {
@@ -30,6 +59,10 @@ struct AddressView: View {
         }
         .navigationTitle("Delivery details")
         .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    func trimWhitespaces(_ string: String) -> String {
+        string.trimmingCharacters(in: .whitespacesAndNewlines)
     }
 }
 
