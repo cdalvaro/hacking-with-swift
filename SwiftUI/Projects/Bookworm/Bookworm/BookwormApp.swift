d86e4f8dea50413cce1a7167d5a0c4f7d0c91659
Carlos Álvaro <carlos@cdalvaro.io>
2023-06-30 16:57:14 +0200

    100 Days of SwiftUI - Day 53
    Project 11 - Part one
    
    Creating a custom component with `@Binding`

---
+++ SwiftUI/Projects/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Carlos Álvaro on 30/6/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/Projects/Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Carlos Álvaro on 30/6/23.
+//
+
+import SwiftUI
+
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
+    var body: some View {
+        Button(title) {
+            isOn.toggle()
+        }
+        .padding()
+        .background(
+            LinearGradient(colors: isOn ? onColors : offColors,
+                           startPoint: .top, endPoint: .bottom)
+        )
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct ContentView: View {
+    @State private var rememberMe = false
+    
+    var body: some View {
+        VStack {
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
