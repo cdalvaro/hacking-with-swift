b428f869873b475757e1bf2b33cbd5adc9b30fe0
Carlos Álvaro <carlos@cdalvaro.io>
2023-07-08 21:46:36 +0200

    100 Days of SwiftUI - Day 55
    Project 11 - Part three
    
    Sorting fetch requests with SortDescriptor

--- SwiftUI/Projects/Bookworm/Bookworm/ContentView.swift
+++ SwiftUI/Projects/Bookworm/Bookworm/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+    @FetchRequest(sortDescriptors: [
+        SortDescriptor(\.title),
+        SortDescriptor(\.author)
+    ]) var books: FetchedResults<Book>
     
     @State private var showingAddScreen = false
     
