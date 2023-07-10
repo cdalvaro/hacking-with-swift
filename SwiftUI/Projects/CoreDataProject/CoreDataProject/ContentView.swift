b6be7020100931e1cd647ce72cfdcdc5fdfc99a3
Carlos Álvaro <carlos@cdalvaro.io>
2023-07-10 18:36:08 +0200

    100 Days of SwiftUI - Day 57
    Project 12 - Part one
    
    Why does \.self work for ForEach?

--- SwiftUI/Projects/CoreDataProject/CoreDataProject/ContentView.swift
+++ SwiftUI/Projects/CoreDataProject/CoreDataProject/ContentView.swift
@@ -7,15 +7,20 @@
 
 import SwiftUI
 
+struct Student: Hashable {
+    let name: String
+}
+
 struct ContentView: View {
+    let students = [
+        Student(name: "Harry Potter"),
+        Student(name: "Hermione Granger")
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(students, id: \.self) { student in
+            Text(student.name)
         }
-        .padding()
     }
 }
 
