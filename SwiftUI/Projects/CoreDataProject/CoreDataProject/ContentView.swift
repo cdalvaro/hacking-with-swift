7672c7f3a60206469a210c5f90bdffc15358710d
Carlos Álvaro <carlos@cdalvaro.io>
2023-07-29 14:07:44 +0200

    100 Days of SwiftUI - Day 58
    Project 12 - Part two
    
    Filtering @FetchRequest using NSPredicate

--- SwiftUI/Projects/CoreDataProject/CoreDataProject/ContentView.swift
+++ SwiftUI/Projects/CoreDataProject/CoreDataProject/ContentView.swift
@@ -5,28 +5,38 @@
 //  Created by Carlos Álvaro on 10/7/23.
 //
 
+import CoreData
 import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe IN %@", ["Aliens", "Star Wars"])) var ships: FetchedResults<Ship>
     
     var body: some View {
         VStack {
-            List(wizards, id: \.self) { wizard in
-                Text(wizard.name ?? "Unknown")
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknonw name")
             }
             
-            Button("Add") {
-                let wizard = Wizard(context: moc)
-                wizard.name = "Harry Potter"
-            }
-            
-            Button("Save") {
-                do {
-                    try moc.save()
-                } catch {
-                    print(error.localizedDescription)
+            Button("Add example") {
+                let ship1 = Ship(context: moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+                
+                let ship2 = Ship(context: moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+                
+                let ship3 = Ship(context: moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star Wars"
+                
+                let ship4 = Ship(context: moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+                
+                if (moc.hasChanges) {
+                    try? moc.save()
                 }
             }
         }
