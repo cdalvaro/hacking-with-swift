0eb77aafca86104b24a116b4b1567f1ebc9b77db
Carlos Álvaro <carlos@cdalvaro.io>
2023-07-30 09:37:32 +0200

    100 Days of SwiftUI - Day 58
    Project 12 - Part two
    
    Dynamically filtering @FetchRequest with SwiftUI (Basic)

---
+++ SwiftUI/Projects/CoreDataProject/CoreDataProject/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Carlos Álvaro on 30/7/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
---
+++ SwiftUI/Projects/CoreDataProject/CoreDataProject/Singer+CoreDataProperties.swift
@@ -0,0 +1,33 @@
+//
+//  Singer+CoreDataProperties.swift
+//  CoreDataProject
+//
+//  Created by Carlos Álvaro on 30/7/23.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Singer {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Singer> {
+        return NSFetchRequest<Singer>(entityName: "Singer")
+    }
+
+    @NSManaged public var firstName: String?
+    @NSManaged public var lastName: String?
+    
+    var wrappedFirstName: String {
+        firstName ?? "Unknown"
+    }
+    
+    var wrappedLastName: String {
+        lastName ?? "Unknown"
+    }
+}
+
+extension Singer : Identifiable {
+
+}
