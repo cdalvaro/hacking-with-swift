69011c3df5d4d292aeb6ddb0f476286e0ebc2251
Carlos Álvaro <carlos@cdalvaro.io>
2024-02-10 16:51:32 +0100

    100 Days of SwiftUI - Day 58
    Project 12 - Part two
    
    Syncing SwiftData with CloudKit

--- SwiftUI/Projects/SwiftDataProject/SwiftDataProject/User.swift
+++ SwiftUI/Projects/SwiftDataProject/SwiftDataProject/User.swift
@@ -10,16 +10,20 @@ import SwiftData
 
 @Model
 class User {
-    var name: String
-    var city: String
-    var joinDate: Date
+    var name: String = "Anonymous"
+    var city: String = "Unknown"
+    var joinDate: Date = .now
 
     // The default deleteRule is .nullify, which means the owner
     // property of each Job object gets set to nil, marking that
     // they have no owner.
     // We are going to change that to be .cascade, which means
     // deleting a User should automatically delete all ther Job objects.
-    @Relationship(deleteRule: .cascade) var jobs = [Job]()
+    @Relationship(deleteRule: .cascade) var jobs: [Job]? = [Job]()
+
+    var unwrappedJobs: [Job] {
+        jobs ?? []
+    }
 
     init(name: String, city: String, joinDate: Date) {
         self.name = name
