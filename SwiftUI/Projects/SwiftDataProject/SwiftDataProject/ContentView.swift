1ae8935985fb5f354b45ce133df5b712dc2c059a
Carlos Álvaro <carlos@cdalvaro.io>
2024-02-10 13:53:56 +0100

    100 Days of SwiftUI - Day 58
    Project 12 - Part two
    
    Dynamically sorting and filtering @Query with SwiftUI

--- SwiftUI/Projects/SwiftDataProject/SwiftDataProject/ContentView.swift
+++ SwiftUI/Projects/SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -12,35 +12,61 @@ let secondsInDay: Double = 60 * 60 * 24
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query(
-        filter: #Predicate<User> { user in
-            // localizedStandardContains is case insensitive
-            user.name.localizedStandardContains("R") &&
-            user.city == "London"
-        },
-        sort: \User.name) var users: [User]
+    @State private var showingUpcomingOnly = false
+    @State private var sortOrder = [
+        SortDescriptor(\User.name),
+        SortDescriptor(\User.joinDate)
+    ]
 
     var body: some View {
         NavigationStack {
-            List(users) { user in
-                Text(user.name)
-            }
-            .navigationTitle("Users")
-            .toolbar {
-                Button("Add Samples", systemImage: "plus") {
-                    try? modelContext.delete(model: User.self)
+            UsersView(miniumJoinDate: showingUpcomingOnly ? .now : .distantPast,
+                      sortOrder: sortOrder)
+                .navigationTitle("Users")
+                .toolbar {
+                    Button("Add Samples", systemImage: "plus") {
+                        try? modelContext.delete(model: User.self)
 
-                    let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(secondsInDay * -10))
-                    let second = User(name: "Rosa Díaz", city: "New York", joinDate: .now.addingTimeInterval(secondsInDay * -5))
-                    let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(secondsInDay * 5))
-                    let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(secondsInDay * 10))
+                        let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(secondsInDay * -10))
+                        let second = User(name: "Rosa Díaz", city: "New York", joinDate: .now.addingTimeInterval(secondsInDay * -5))
+                        let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(secondsInDay * 5))
+                        let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(secondsInDay * 10))
 
-                    modelContext.insert(first)
-                    modelContext.insert(second)
-                    modelContext.insert(third)
-                    modelContext.insert(fourth)
+                        modelContext.insert(first)
+                        modelContext.insert(second)
+                        modelContext.insert(third)
+                        modelContext.insert(fourth)
+                    }
+
+                    Button(showingUpcomingOnly ? "Show Everyone" : "Show Upcoming") {
+                        showingUpcomingOnly.toggle()
+                    }
+                    
+                    // Menu lets you create menus in the navigation bar, and you can
+                    // place buttons, pickers, and more inside ther.
+                    // In this case, Menu hides the Picker title and only shows its
+                    // options.
+                    Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                        Picker("Sort", selection: $sortOrder) {
+                            // `tag` lets attach specific values of our choosing
+                            // to each picker option.
+                            // In this case, the tag of each option has its
+                            // own `SortDescriptor` array, and SwiftUI will assign
+                            // that tag to the `sortOrder` property automatically.
+                            Text("Sort by Name")
+                                .tag([
+                                    SortDescriptor(\User.name),
+                                    SortDescriptor(\User.joinDate)
+                                ])
+                            
+                            Text("Sort by Join Date")
+                                .tag([
+                                    SortDescriptor(\User.joinDate),
+                                    SortDescriptor(\User.name)
+                                ])
+                        }
+                    }
                 }
-            }
         }
     }
 }
