00a4cda326f4efb64c577cf9cb16e9ce2151714b
Carlos Álvaro <carlos@cdalvaro.io>
2024-03-31 15:52:23 +0200

    100 Days of SwiftUI - Day 63
    Project 13 - Part two
    
    Showing empty states with ContentUnavailableView

--- SwiftUI/Projects/Instafilter/Instafilter/ContentView.swift
+++ SwiftUI/Projects/Instafilter/Instafilter/ContentView.swift
@@ -5,50 +5,20 @@
 //  Created by Carlos Álvaro on 14/2/24.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
+        ContentUnavailableView {
+            Label("No snippets", systemImage: "swift")
+        } description: {
+            Text("You don't have any saved snippets yet")
+        } actions: {
+            Button("Create snippet") {
+                // create a snippet
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .onAppear(perform: loadImage)
-    }
-    
-    func loadImage() {
-        let inputImage = UIImage(resource: .example)
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.pixellate()
-        
-        currentFilter.inputImage = beginImage
-        let amount = 1.0
-        let inputKeys = currentFilter.inputKeys
-        
-        if inputKeys.contains(kCIInputIntensityKey) {
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
-        }
-        
-        if inputKeys.contains(kCIInputRadiusKey) {
-            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
-        }
-        
-        if inputKeys.contains(kCIInputScaleKey) {
-            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
-        }
-        
-        guard let outputImage = currentFilter.outputImage else { return }
-        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
-        
-        let uiImage = UIImage(cgImage: cgImage)
-        image = Image(uiImage: uiImage)
     }
 }
 
