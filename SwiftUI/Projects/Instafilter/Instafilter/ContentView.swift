e3631e723ed6f671db6f376814c4ca2ca9aa11a4
Carlos Álvaro <carlos@cdalvaro.io>
2024-04-01 19:28:04 +0200

    100 Days of SwiftUI - Day 65
    Project 13 - Part four
    
    Building our basic UI

--- SwiftUI/Projects/Instafilter/Instafilter/ContentView.swift
+++ SwiftUI/Projects/Instafilter/Instafilter/ContentView.swift
@@ -5,17 +5,48 @@
 //  Created by Carlos Álvaro on 14/2/24.
 //
 
-import StoreKit
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.requestReview) var requestReview
-    
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+
     var body: some View {
-        Button("Leave a review") {
-            requestReview()
+        NavigationStack {
+            VStack {
+                Spacer()
+
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No picture",
+                                           systemImage: "photo.badge.plus",
+                                           description: Text("Tap to import a photo"))
+                }
+
+                Spacer()
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+
+                    Spacer()
+
+                    // share the picture
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
     }
+
+    func changeFilter() {}
 }
 
 #Preview {
