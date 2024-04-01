92d28ca469c475bfe2c53a564e4701ed170b7f11
Carlos Álvaro <carlos@cdalvaro.io>
2024-04-01 17:55:51 +0200

    100 Days of SwiftUI - Day 64
    Project 13 - Part three
    
    How to ask the user to leave an App Store review

--- SwiftUI/Projects/Instafilter/Instafilter/ContentView.swift
+++ SwiftUI/Projects/Instafilter/Instafilter/ContentView.swift
@@ -5,30 +5,16 @@
 //  Created by Carlos Álvaro on 14/2/24.
 //
 
+import StoreKit
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.requestReview) var requestReview
+    
     var body: some View {
-        Spacer()
-        
-        ShareLink(item: URL(string: "https://cdalvaro.io")!,
-                  subject: Text("I'm Carlos!"),
-                  message: Text("Checkout my profile"))
-        
-        Spacer()
-        
-        ShareLink(item: URL(string: "https://www.hackwingwithswift.com")!) {
-            Label("Spread the word about Swift", systemImage: "swift")
+        Button("Leave a review") {
+            requestReview()
         }
-        
-        Spacer()
-        
-        let example = Image(.example)
-        ShareLink(item: example, preview: SharePreview("Basic Apple Guy Picture", image: example)) {
-            Label("Click to share", systemImage: "mug")
-        }
-        
-        Spacer()
     }
 }
 
