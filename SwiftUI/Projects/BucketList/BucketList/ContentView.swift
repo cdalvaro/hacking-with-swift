5b6ccb66962f771b3f7364bd899e03558eedaaa7
Carlos Álvaro <carlos@cdalvaro.io>
2024-07-29 12:01:16 +0200

    100 Days of SwiftUI - Day 68
    Project 14 - Part one
    
    Writing data to the documents directory

--- SwiftUI/Projects/BucketList/BucketList/ContentView.swift
+++ SwiftUI/Projects/BucketList/BucketList/ContentView.swift
@@ -7,26 +7,19 @@
 
 import SwiftUI
 
-struct User: Identifiable, Comparable {
-    let id = UUID()
-    let firstName: String
-    let lastName: String
-
-    static func < (lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
-    }
-}
-
 struct ContentView: View {
-    let users = [
-        User(firstName: "Arnold", lastName: "Rimmer"),
-        User(firstName: "Kristine", lastName: "Kochanski"),
-        User(firstName: "David", lastName: "Lister")
-    ].sorted()
-
     var body: some View {
-        List(users) { user in
-            Text("\(user.firstName), \(user.lastName)")
+        Button("Read and Write") {
+            let data = Data("Test Message".utf8)
+            let url = URL.documentsDirectory.appending(path: "message.txt")
+
+            do {
+                try data.write(to: url, options: [.atomic, .completeFileProtection])
+                let input = try String(contentsOf: url)
+                print(input)
+            } catch {
+                print(error.localizedDescription)
+            }
         }
     }
 }
