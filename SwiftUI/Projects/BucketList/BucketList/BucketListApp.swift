d550513f35aff01c3b057ff3e5006e23690f6710
Carlos Álvaro <carlos@cdalvaro.io>
2024-07-29 11:50:21 +0200

    100 Days of SwiftUI - Day 68
    Project 14 - Part one
    
    Adding conformance to `Comparable` for custom types

---
+++ SwiftUI/Projects/BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Carlos Álvaro on 29/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
