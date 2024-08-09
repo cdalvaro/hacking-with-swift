7b5da6766425369623838555b0f2ad8cbf6762a5
Carlos Álvaro <carlos@cdalvaro.io>
2024-08-09 18:11:27 +0200

    100 Days of SwiftUI - Day 72
    Project 14 - Part five
    
    Introducing MVVM into your SwiftUI project

---
+++ SwiftUI/Projects/BucketList/BucketList/ContentView-ViewModel.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView-ViewModel.swift
+//  BucketList
+//
+//  Created by Carlos Álvaro on 9/8/24.
+//
+
+import CoreLocation
+import Foundation
+import MapKit
+
+extension ContentView {
+    @Observable
+    class ViewModel {
+        private(set) var locations: [Location]
+        var selectedPlace: Location?
+
+        let savePath = URL.documentsDirectory.appending(path: "SavedPlaces")
+
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            } catch {
+                print("Unable to save data.")
+            }
+        }
+
+        func addLocation(at point: CLLocationCoordinate2D) {
+            let newLocation = Location(
+                id: UUID(),
+                name: "New Location",
+                description: "",
+                latitude: point.latitude,
+                longitude: point.longitude
+            )
+            locations.append(newLocation)
+            save()
+        }
+
+        func updateLocation(location: Location) {
+            guard let selectedPlace else { return }
+
+            if let index = locations.firstIndex(of: selectedPlace) {
+                locations[index] = location
+                save()
+            }
+        }
+    }
+}
