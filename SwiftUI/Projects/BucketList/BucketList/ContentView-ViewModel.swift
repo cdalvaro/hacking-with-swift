10a44ceb552289c80b33a9d17b46cdfa2a809263
Carlos Álvaro <carlos@cdalvaro.io>
2024-08-09 18:23:48 +0200

    100 Days of SwiftUI - Day 72
    Project 14 - Part five
    
    Locking our UI behind Face ID

--- SwiftUI/Projects/BucketList/BucketList/ContentView-ViewModel.swift
+++ SwiftUI/Projects/BucketList/BucketList/ContentView-ViewModel.swift
@@ -7,6 +7,7 @@
 
 import CoreLocation
 import Foundation
+import LocalAuthentication
 import MapKit
 
 extension ContentView {
@@ -14,6 +15,7 @@ extension ContentView {
     class ViewModel {
         private(set) var locations: [Location]
         var selectedPlace: Location?
+        var isUnlocked = false
 
         let savePath = URL.documentsDirectory.appending(path: "SavedPlaces")
 
@@ -53,6 +55,26 @@ extension ContentView {
             if let index = locations.firstIndex(of: selectedPlace) {
                 locations[index] = location
                 save()
+            }
+        }
+
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Please authenticate yourself to unlock your places."
+
+                context
+                    .evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, _ in
+                        if success {
+                            self.isUnlocked = true
+                        } else {
+                            // error
+                        }
+                    }
+            } else {
+                // no biometrics
             }
         }
     }
