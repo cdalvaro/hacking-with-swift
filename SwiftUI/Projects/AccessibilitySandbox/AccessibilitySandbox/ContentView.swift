5099f58832a13977ec6184c780200aec0188c16b
Carlos Álvaro <carlos@cdalvaro.io>
2024-08-17 21:14:05 +0200

    100 Days of SwiftUI - Day 75
    Project 15 - Part two
    
    Handling voice input in SwiftUI

--- SwiftUI/Projects/AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
+++ SwiftUI/Projects/AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -8,36 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var value = 10
-
     var body: some View {
-        VStack {
-            Text("Value: \(value)")
-
-            Button("Increment") {
-                value += 1
-            }
-
-            Button("Decrement") {
-                value -= 1
-            }
+        Button("John Fitzgerald Kennedy") {
+            print("Button tapped")
         }
-        .accessibilityElement()
-        .accessibilityLabel("Value")
-        .accessibilityValue(String(value))
-        .accessibilityAdjustableAction { direction in
-            // That lets users select the whole VStack to have “Value: 10” read out,
-            // but then they can swipe up or down to manipulate the value and have
-            // just the numbers read out.
-            switch direction {
-            case .increment:
-                value += 1
-            case .decrement:
-                value -= 1
-            default:
-                print("Not handled")
-            }
-        }
+        .accessibilityInputLabels(["John Fitzgerald Kennedy", "Kennedy", "JFK"])
     }
 }
 
