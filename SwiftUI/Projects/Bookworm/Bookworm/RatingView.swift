df88bc68d267bd9299caae5523e4ba5046207b3e
Carlos Álvaro <carlos@cdalvaro.io>
2024-08-21 20:34:24 +0200

    100 Days of SwiftUI - Day 75
    Project 15 - Part two
    
    Fixing Bookworm

--- SwiftUI/Projects/Bookworm/Bookworm/RatingView.swift
+++ SwiftUI/Projects/Bookworm/Bookworm/RatingView.swift
@@ -9,37 +9,52 @@ import SwiftUI
 
 struct RatingView: View {
     @Binding var rating: Int
-    
+
     var label = ""
     var maximumRating = 5
-    
+
     var offImage: Image?
     var onImage = Image(systemName: "star.fill")
-    
+
     var offColor = Color.gray
     var onColor = Color.yellow
-    
+
     var body: some View {
         HStack {
             if label.isEmpty == false {
                 Text(label)
             }
-            
-            ForEach(1...maximumRating, id: \.self) { number in
-                image(for: number)
-                    .foregroundColor(number > rating ? offColor : onColor)
-                    .onTapGesture {
-                        rating = number
-                    }
+
+            ForEach(1 ... maximumRating, id: \.self) { number in
+                Button {
+                    rating = number
+                } label: {
+                    image(for: number)
+                        .foregroundColor(number > rating ? offColor : onColor)
+                }
+            }
+        }
+        .buttonStyle(.plain)
+        .accessibilityElement()
+        .accessibilityLabel(label)
+        .accessibilityValue(String(AttributedString(localized: "^[\(rating) \("star")](inflect: true)").characters))
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                if rating < maximumRating { rating += 1 }
+            case .decrement:
+                if rating > 1 { rating -= 1 }
+            default:
+                break
             }
         }
     }
-    
+
     func image(for number: Int) -> Image {
         if number > rating {
             return offImage ?? onImage
         }
-        
+
         return onImage
     }
 }
