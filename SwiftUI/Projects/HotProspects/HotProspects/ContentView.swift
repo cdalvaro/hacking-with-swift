2427dd8b57f78221e8033ad44e788a78bf586d31
Carlos Álvaro <carlos@cdalvaro.io>
2024-12-22 09:40:33 +0100

    100 Days of SwiftUI - Day 80
    Project 16 - Part two
    
    Understanding Swift's Result type

--- SwiftUI/Projects/HotProspects/HotProspects/ContentView.swift
+++ SwiftUI/Projects/HotProspects/HotProspects/ContentView.swift
@@ -8,23 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedTab = "One"
+    @State private var output = ""
 
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Button("Show Tab 2") {
-                selectedTab = "Two"
+        Text(output)
+            .task {
+                await fetchReadings()
             }
-            .tabItem {
-                Label("One", systemImage: "star")
-            }
-            .tag("One")
+    }
 
-            Text("Tab 2")
-                .tabItem {
-                    Label("Two", systemImage: "circle")
-                }
-                .tag("Two")
+    func fetchReadings() async {
+        let fetchTask = Task {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
+        }
+
+        let result = await fetchTask.result
+
+        // -- Way one
+        do {
+            output = try result.get()
+        } catch {
+            output = "Error: \(error.localizedDescription)"
+        }
+
+        // -- Way two
+        switch result {
+        case let .success(str):
+            output = str
+        case let .failure(error):
+            output = "Error: \(error.localizedDescription)"
         }
     }
 }
