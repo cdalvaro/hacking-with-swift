f25b0a11107f7991aa8fedc8e01fb91b89b707d0
Carlos Álvaro <carlos@cdalvaro.io>
2025-01-11 10:45:53 +0100

    100 Days of SwiftUI - Day 81
    Project 16 - Part three
    
    Scheduling local notifications

--- SwiftUI/Projects/HotProspects/HotProspects/ContentView.swift
+++ SwiftUI/Projects/HotProspects/HotProspects/ContentView.swift
@@ -6,24 +6,35 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
-    @State private var backgroundColor = Color.red
-
     var body: some View {
-        List {
-            Text("Taylor Swift")
-                .swipeActions {
-                    Button("Delete", systemImage: "minus.circle", role: .destructive) {
-                        print("Delete")
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter
+                    .current()
+                    .requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                        if success {
+                            print("All set!")
+                        } else if let error {
+                            print(error.localizedDescription)
+                        }
                     }
-                }
-                .swipeActions(edge: .leading) {
-                    Button("Pin", systemImage: "pin") {
-                        print("Pinning")
-                    }
-                    .tint(.orange)
-                }
+            }
+
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "It looks hungry"
+                content.sound = UNNotificationSound.default
+
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+                UNUserNotificationCenter.current().add(request)
+            }
         }
     }
 }
