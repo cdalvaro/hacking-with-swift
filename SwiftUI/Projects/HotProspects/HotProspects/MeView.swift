cbafc94bd3e473c34c22af6122701ea1a610f19d
Carlos Álvaro <carlos@cdalvaro.io>
2025-01-12 19:11:01 +0100

    100 Days of SwiftUI - Day 83
    Project 16 - Part five
    
    Generating and scaling up a QR code

--- SwiftUI/Projects/HotProspects/HotProspects/MeView.swift
+++ SwiftUI/Projects/HotProspects/HotProspects/MeView.swift
@@ -5,11 +5,47 @@
 //  Created by Carlos Álvaro on 11/1/25.
 //
 
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
+    @AppStorage("name") private var name = "Anonymous"
+    @AppStorage("emailAddress") private var emailAddress = "you@example.com"
+
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+
+                TextField("Email address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+            .navigationTitle("Your code")
+        }
+    }
+
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
