0dcc017f557bfe09f6e1dff0a72009de06449d34
Carlos Álvaro <carlos@cdalvaro.io>
2025-01-12 08:52:41 +0100

    100 Days of SwiftUI - Day 82
    Project 16 - Part four
    
    Dynamically filtering our SwiftData query

--- SwiftUI/Projects/HotProspects/HotProspects/ProspectsView.swift
+++ SwiftUI/Projects/HotProspects/HotProspects/ProspectsView.swift
@@ -30,18 +30,40 @@ struct ProspectsView: View {
 
     var body: some View {
         NavigationStack {
-            Text("People: \(prospects.count)")
-                .navigationTitle(title)
-                .toolbar {
-                    Button("Scan", systemImage: "qrcode.viewfinder") {
-                        let prospect = Prospect(
-                            name: "Carlos Álvaro",
-                            emailAddress: "github@cdalvaro.io",
-                            isContacted: false
-                        )
-                        modelContext.insert(prospect)
-                    }
+            List(prospects) { prospect in
+                VStack(alignment: .leading) {
+                    Text(prospect.name)
+                        .font(.headline)
+
+                    Text(prospect.emailAddress)
+                        .foregroundStyle(.secondary)
                 }
+            }
+            .navigationTitle(title)
+            .toolbar {
+                Button("Scan", systemImage: "qrcode.viewfinder") {
+                    let prospect = Prospect(
+                        name: "Carlos Álvaro",
+                        emailAddress: "github@cdalvaro.io",
+                        isContacted: false
+                    )
+                    modelContext.insert(prospect)
+                }
+            }
+        }
+    }
+
+    init(filter: FilterType) {
+        self.filter = filter
+
+        if filter != .none {
+            let showContactedOnly = filter == .contacted
+
+            _prospects = Query(
+                filter: #Predicate {
+                    $0.isContacted == showContactedOnly
+                }, sort: [SortDescriptor(\Prospect.name)]
+            )
         }
     }
 }
