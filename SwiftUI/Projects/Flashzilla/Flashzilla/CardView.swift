5df45386aebe764e2959c6266f4b7ac7d58b172c
Carlos Álvaro <carlos@cdalvaro.io>
2025-03-25 22:48:32 +0100

    100 Days of SwiftUI - Day 88
    Project 17 - Part three
    
    Moving views with DragGesture and offset()

--- SwiftUI/Projects/Flashzilla/Flashzilla/CardView.swift
+++ SwiftUI/Projects/Flashzilla/Flashzilla/CardView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct CardView: View {
     @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
+
     let card: Card
+    var removal: (() -> Void)?
 
     var body: some View {
         ZStack {
@@ -32,6 +35,20 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(Double(offset.width / 5)))
+        .offset(x: offset.width * 5)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(DragGesture()
+            .onChanged { gesture in
+                offset = gesture.translation
+            }
+            .onEnded { _ in
+                if abs(offset.width) > 100 {
+                    removal?()
+                } else {
+                    offset = .zero
+                }
+            })
         .onTapGesture {
             isShowingAnswer.toggle()
         }
--- SwiftUI/Projects/Flashzilla/Flashzilla/ContentView.swift
+++ SwiftUI/Projects/Flashzilla/Flashzilla/ContentView.swift
@@ -26,12 +26,20 @@ struct ContentView: View {
             VStack {
                 ZStack {
                     ForEach(0 ..< cards.count, id: \.self) { index in
-                        CardView(card: cards[index])
-                            .stacked(at: index, in: cards.count)
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: cards.count)
                     }
                 }
             }
         }
+    }
+
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
     }
 }
 
