213bcaa131ce627d1012de28e6e1d89578827340
Carlos Álvaro <carlos@cdalvaro.io>
2025-03-28 18:38:59 +0100

    100 Days of SwiftUI - Day 89
    Project 17 - Part four
    
    Counting down with a Timer

--- SwiftUI/Projects/Flashzilla/Flashzilla/ContentView.swift
+++ SwiftUI/Projects/Flashzilla/Flashzilla/ContentView.swift
@@ -19,6 +19,12 @@ struct ContentView: View {
 
     @State private var cards = [Card](repeating: .example, count: 10)
 
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
+
     var body: some View {
         ZStack {
             Image(.background)
@@ -26,6 +32,14 @@ struct ContentView: View {
                 .ignoresSafeArea()
 
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+
                 ZStack {
                     ForEach(0 ..< cards.count, id: \.self) { index in
                         CardView(card: cards[index]) {
@@ -60,6 +74,16 @@ struct ContentView: View {
                     .padding()
                 }
             }
+        }
+        .onReceive(timer) { _ in
+            guard isActive else { return }
+
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) {
+            isActive = scenePhase == .active
         }
     }
 
