814aab8e17e23e8a7120fad38a823b73fd064378
Carlos Álvaro <carlos@cdalvaro.io>
2025-06-18 19:00:47 +0200

    100 Days of SwiftUI - Day 93
    Project 18 - Part two
    
    ScrollView effects using GeometryReader

--- SwiftUI/Projects/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ SwiftUI/Projects/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,59 +7,66 @@
 
 import SwiftUI
 
-struct OuterView: View {
+struct VerticalView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        VStack {
-            Text("Top")
-
-            InnerView()
-                .background(.green)
-
-            Text("Bottom")
+        GeometryReader { geometryFullView in
+            ScrollView {
+                ForEach(0 ..< 50) { index in
+                    GeometryReader { geometry in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % colors.count])
+                            .rotation3DEffect(
+                                .degrees(geometry.frame(in: .global).minY - geometryFullView.size.height / 2) / 5,
+                                axis: (x: 0, y: 1, z: 0)
+                            )
+                    }
+                    .frame(height: 40)
+                }
+            }
         }
     }
 }
 
-struct InnerView: View {
+struct HorizontalView: View {
     var body: some View {
-        HStack {
-            Text("Left")
-
-            GeometryReader { geometry in
-                Text("Center")
-                    .background(.blue)
-                    .onTapGesture {
-                        /**
-                         Which coordinate space you want to use depends on what question you want to answer:
-
-                         - Want to know where this view is on the screen? Use the global space.
-                         - Want to know where this view is relative to its parent? Use the local space.
-                         - What to know where this view is relative to some other view? Use a custom space.
-                         */
-                        print(
-                            "Global center: \(geometry.frame(in: .global).midX) " +
-                                "x \(geometry.frame(in: .global).midY)"
-                        )
-                        print("Local center: \(geometry.frame(in: .local).midX) " +
-                            "x \(geometry.frame(in: .local).midY)")
-                        print(
-                            "Custom center: \(geometry.frame(in: .named("Custom")).midX) " +
-                                "x \(geometry.frame(in: .named("Custom")).midY)"
-                        )
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(0 ..< 20) { number in
+                    GeometryReader { geometry in
+                        Text("Number \(number)")
+                            .font(.largeTitle)
+                            .padding()
+                            .background(.red)
+                            .rotation3DEffect(
+                                .degrees(-geometry.frame(in: .global).minX) / 8,
+                                axis: (x: 0, y: 1, z: 0)
+                            )
+                            .frame(width: 200, height: 200)
                     }
+                    .frame(width: 200, height: 200)
+                }
             }
-            .background(.orange)
-
-            Text("Right")
         }
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        OuterView()
-            .background(.red)
-            .coordinateSpace(name: "Custom")
+        TabView {
+            VerticalView()
+                .tabItem {
+                    Label("Vertical", systemImage: "1.circle")
+                }
+
+            HorizontalView()
+                .tabItem {
+                    Label("Horizontal", systemImage: "2.circle")
+                }
+        }
     }
 }
 
