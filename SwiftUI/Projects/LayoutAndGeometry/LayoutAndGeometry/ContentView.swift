a307323439340c9a2f11607f54d7e2735dfbc4e5
Carlos Álvaro <carlos@cdalvaro.io>
2025-06-18 17:50:16 +0200

    100 Days of SwiftUI - Day 92
    Project 18 - Part one
    
    How to create a custom alignment guide

--- SwiftUI/Projects/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ SwiftUI/Projects/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,42 +7,39 @@
 
 import SwiftUI
 
+extension VerticalAlignment {
+    enum MidAccountAndName: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[.top]
+        }
+    }
+
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Live long and prosper")
-            .frame(width: 300, height: 300, alignment: .topLeading)
-
-        HStack(alignment: .lastTextBaseline) {
-            Text("Live")
-                .font(.caption)
-
-            Text("long")
-
-            Text("and")
-                .font(.title)
-
-            Text("prospect")
-                .font(.largeTitle)
-        }
-
-        VStack(alignment: .leading) {
-            Text("Hello, world!")
-                // We are returning here the trailing edge for the leading edge
-                .alignmentGuide(.leading) { dim in dim[.trailing] }
-            Text("This is a longer line of text")
-
-            Spacer()
-
-            ForEach(0 ..< 10) { position in
-                Text("Number \(position)")
-                    .alignmentGuide(.leading) { _ in
-                        Double(position) * -10
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@cdalvaro")
+                    .alignmentGuide(.midAccountAndName) { dim in
+                        dim[VerticalAlignment.center]
                     }
+
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+
+            VStack {
+                Text("Full name:")
+                Text("CARLOS ÁLVARO")
+                    .alignmentGuide(.midAccountAndName) { dim in
+                        dim[VerticalAlignment.center]
+                    }
+                    .font(.largeTitle)
             }
         }
-        .background(.yellow)
-        .frame(width: 400, height: 400)
-        .background(.blue)
     }
 }
 
